85a1baf5158167c171d1424b1712f3b171873359
Mario Amaral <mario.fts@gmail.com>
2018-03-27 17:44:38 -0300

    Configure a new docker image, with support to drafts

--- run-local.sh
+++ run-local.sh
@@ -1,3 +1,3 @@
 docker rm $(docker rm -f local-jekyll) >> dev/null
 
-docker run -t --rm --name local-jekyll -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
+docker run -t --rm --name local-jekyll -v "$PWD":/site -p "4000:4000" andredumas/github-pages serve --watch --draft
